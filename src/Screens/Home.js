ffa95fea9622b77334a4d7809f58235d9956436a
Hannoune Othmane <57452112+othmanehannoun@users.noreply.github.com>
2021-10-28 16:40:50 +0100

    new update

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,38 +1,32 @@
 import React from 'react'
+import Navbar from '../components/navbar'
+import {Link} from 'react-router-dom';
 import '../css/App.css'
 
 function Home() {
   return (
-    <div>
-        <div className="header-blue">
-          <nav className="navbar navbar-dark navbar-expand-md navigation-clean-search">
-            <div className="container"><a className="navbar-brand" href="#">Company Name</a><button className="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon" /></button>
-              <div className="collapse navbar-collapse" id="navcol-1">
-                <ul className="nav navbar-nav">
-                  <li className="nav-item" role="presentation"><a className="nav-link active" href="#">Link</a></li>
-                  <li className="dropdown"><a className="dropdown-toggle nav-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#">Dropdown </a>
-                    <div className="dropdown-menu" role="menu"><a className="dropdown-item" role="presentation" href="#">First Item</a><a className="dropdown-item" role="presentation" href="#">Second Item</a><a className="dropdown-item" role="presentation" href="#">Third Item</a></div>
-                  </li>
-                </ul>
-                <form className="form-inline mr-auto" target="_self">
-                  <div className="form-group"><label htmlFor="search-field"><i className="fa fa-search" /></label><input className="form-control search-field" type="search" name="search" id="search-field" /></div>
-                </form><span className="navbar-text"> <a href="#" className="login">Log In</a></span><a className="btn btn-light action-button" role="button" href="#">Sign Up</a></div>
-            </div>
-          </nav>
-          <div className="container hero">
-            <div className="row">
-              <div className="col-12 col-lg-6 col-xl-5 offset-xl-1">
-                <h1>The revolution is here.</h1>
-                <p>Mauris egestas tellus non ex condimentum, ac ullamcorper sapien dictum. Nam consequat neque quis sapien viverra convallis. In non tempus lorem. </p><button className="btn btn-light btn-lg action-button" type="button">Learn More</button></div>
-              <div className="col-md-5 col-lg-5 offset-lg-1 offset-xl-0 d-none d-lg-block phone-holder">
-                <div className="iphone-mockup"><img src="assets/img/iphone.svg" className="device" />
-                  <div className="screen" />
-                </div>
-              </div>
-            </div>
-          </div>
+    <div className="container">
+
+      <Navbar />
+
+      
+       <div className="row header">
+       <div className="col-md-6 content1-left">
+          <h3>Le carnet de crédit sur téléphone qui vous facilite la vie.</h3>
+          <p style={{fontSize:'22px', color:'rgb(47 47 47)'}}>Enregistrez simplement vos transactions, directement sur votre téléphone et collectez votre argent plus facilement.</p>
+          <p>Cliquez ici, pour télécharger l’application</p>
+          <Link to="">
+            <img src="../../assets/img/googleplay.svg" alt=""/>
+          </Link>
         </div>
+        <div className="col-md-6 content1-right">
+          <img src='../../assets/img/iphone.svg' alt="é"/>
+        </div>
+       </div>
+       
       </div>
+
+   
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+//import './css/App.css';
+
+
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+    <div className="container">
+        <img src="../../assets/img/work.png" width="5%" alt="xxx"/>
+        <Link className="ml-3 navbar-brand" to="/">Karny</Link>
+        
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+            <Link className="nav-link" to="#">Home</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="#">About</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link" to="">Contact us</Link>
+            </li>
+        </ul>
+
+        <div class="d-flex">
+            <button class="themeBtn4">Télecharger L'app <i class="fa fa-arrow-circle-right"></i></button>       
+        </div>
+
+        </div>
+    </div>
+  </nav>
+  );
+}
+
+
+export default Navbar;
+
+
