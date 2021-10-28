1e30e90181070f00b469adcab2bf380cbfbb9e46
Hannoune Othmane <57452112+othmanehannoun@users.noreply.github.com>
2021-10-28 01:23:18 +0100

    new update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import React from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+//import './css/App.css';
 
-import './App.css';
+// user Routes
+import Home from './Screens/Home'
+// import About from './Screens/ProductDetails';
+// import Contact from './Screens/Contact'
+
 
 function App() {
   return (
-    <div className="App">
-      <p className="hello">Hello World</p>
-    </div>
+    
+        <BrowserRouter>
+        <Switch>
+            <Route path = "/" exact component={Home}/>
+            {/* <Route path = "/contact" exact component={Contact} />  */}
+            {/* <Route path = "/products/:idcat" exact component={fetchByCategory} />  */}
+      
+        </Switch>
+    
+      </BrowserRouter>
+    
   );
 }
 
+
 export default App;
+
+
